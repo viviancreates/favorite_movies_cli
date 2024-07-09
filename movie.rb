9af49862dc795246abc22c16fe11cc41944f00a4
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-07-09 04:54:19 +0000

    Add class for adding a movie, including creating getters and setters, initialize, create output

---
+++ movie.rb
@@ -0,0 +1,17 @@
+class Movie
+  attr_accessor :title :director :year :genre :runtime :review
+    
+  def initialize
+    @title = title
+    @director = director
+    @year = year
+    @genre = genre
+    @runtime = runtime
+    @review = review
+  end
+
+  def to_s
+    "Title: #{@title}, Director: #{@director}, Year Made: #{@year}, Genre: #{@genre}, Runtime: #{@runtime}, My Review: #{@review}"
+  end
+
+end
