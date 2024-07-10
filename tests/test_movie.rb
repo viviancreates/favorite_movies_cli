9927107f7d27b46f5810359242264473f9049920
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-07-10 05:46:54 +0000

    Add example test

--- tests/test_movie.rb
+++ tests/test_movie.rb
@@ -1,2 +1,11 @@
 require 'minitest/autorun'
-require './movie'
+require '../movie'
+
+class TestMovie < Minitest::Test
+def test_addmovie
+  movie = Movie.new
+
+  assert_equal "Title: Dune, Director: Denis Villeneuve, Year Released: 2021, Genre: Sci Fi, My Review: Hated it at first, but then loved it after rewatching in 2024.", movie.add("Title: Dune, Director: Denis Villeneuve, Year Released: 2021, Genre: Sci Fi, My Review: Hated it at first, but then loved it after rewatching in 2024."), "Movie addition failed"
+
+end
+end
