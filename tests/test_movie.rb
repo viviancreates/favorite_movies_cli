b98121265e5c84449113c8568f88102b3843e9cc
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-07-10 05:59:07 +0000

    No errors in test popping up, all assertions added

--- tests/test_movie.rb
+++ tests/test_movie.rb
@@ -1,11 +1,15 @@
 require 'minitest/autorun'
-require '../movie'
+require_relative '../movie'
 
 class TestMovie < Minitest::Test
 def test_addmovie
-  movie = Movie.new
+  movie = Movie.new("Title: Dune",  "Director: Denis Villeneuve", "Year Released: 2021", "Genre: Sci Fi", "My Review: Hated it at first, but then loved it after rewatching in 2024.")
 
-  assert_equal "Title: Dune, Director: Denis Villeneuve, Year Released: 2021, Genre: Sci Fi, My Review: Hated it at first, but then loved it after rewatching in 2024.", movie.add("Title: Dune, Director: Denis Villeneuve, Year Released: 2021, Genre: Sci Fi, My Review: Hated it at first, but then loved it after rewatching in 2024."), "Movie addition failed"
+  assert_equal "Title: Dune", movie.title, "Movie title addition failed"
+  assert_equal "Director: Denis Villeneuve", movie.director, "Movie director addition failed"
+  assert_equal "Year Released: 2021", movie.year, "Movie year addition failed"
+  assert_equal "Genre: Sci Fi", movie.genre, "Movie genre addition failed"
+  assert_equal "My Review: Hated it at first, but then loved it after rewatching in 2024.", movie.review, "Movie review addition failed"
 
 end
 end
